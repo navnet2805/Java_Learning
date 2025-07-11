821dc9b0627c068cb64c289870bba6967a4b9354
navnet2805 <naveen.rp.2805@gmail.com>
2025-07-11 09:40:19 +0530

    Update FindNumberOfDigits.java

--- src/FindNumberOfDigits.java
+++ src/FindNumberOfDigits.java
@@ -3,8 +3,8 @@ public class FindNumberOfDigits {
         int num = 03445;
         int count =0;
         while(num!=0){
-            num = num/10;
-            count++;
+            num = num/10; // I am dividing the number by 10 and assiging the result to the same 'num' so that it will decrease the count of digits by 1.
+            count++; //
         }
         System.out.println("The number of digits in the number is "+count);
     }
